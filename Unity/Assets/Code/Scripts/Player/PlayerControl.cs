0da9f0d17db9e847cf3e74ebbbbea2ec0ea9111b
Tanguy Rossel <tanguy.rossel@camptocamp.com>
2024-04-20 01:24:21 +0200

    feat(player): add orientation

--- Unity/Assets/Code/Scripts/Player/PlayerControl.cs
+++ Unity/Assets/Code/Scripts/Player/PlayerControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,10 +18,15 @@ public class PlayerControl : MonoBehaviour
 
     public InputActionReference move;   
 
-    private Vector2 cylindricalMoveDirection = new Vector2(1.0f, 1.0f).normalized;
+    private Vector2 cylindricalMoveDirection = new Vector2(1.0f, 0.0f).normalized;
 
     private Vector2 cylindricalPosition = Vector2.zero;
+    
+    private Vector3 lookForward;
 
+    private Vector3 lookRight;
+
+    private Vector3 lookUp;
 
 
     // Start is called before the first frame update
@@ -50,10 +56,27 @@ public class PlayerControl : MonoBehaviour
         Vector2 velocity = cylindricalMoveDirection * moveSpeed;
         cylindricalPosition += velocity;
 
-        Vector3 newPosition = map.To3dVector(cylindricalPosition);
+        (Vector3 newPosition, Vector3 normal) = map.cylindricalTo3d(cylindricalPosition);
+        Vector3 lookForwardTmp = (newPosition - transform.position).normalized;
 
+
+        lookUp = normal;
+        lookRight = Vector3.Cross(normal, lookForwardTmp).normalized;
+        lookForward = Vector3.Cross(lookRight, normal).normalized;
         transform.position = newPosition;
-        
+        transform.rotation = Quaternion.LookRotation(lookForward, normal);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + 3 * lookForward);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + 3 * lookRight);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + 3 * lookUp);
 
 
     }
