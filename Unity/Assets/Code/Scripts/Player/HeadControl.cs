0d3b38ea5b5b1a0669169749620afea0a6ad468f
gael rubin <concilasplebisll@gmail.com>
2024-04-20 16:11:19 +0200

    add PlayerGael2

--- Unity/Assets/Code/Scripts/Player/HeadControl.cs
+++ Unity/Assets/Code/Scripts/Player/HeadControl.cs
@@ -17,9 +17,7 @@ public class HeadControl : MonoBehaviour
 
     private List<GameObject> bodyList = new List<GameObject>();
     public GameObject bodyPrefab; // Référence vers le prefab de corps à instancier
-    public float bodySpacing = 1f;
-
-    private int bodyCount = 1;
+    public float bodySpacing = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -45,30 +43,20 @@ public class HeadControl : MonoBehaviour
         //Debug.Log(positionList.Count);
     }
 
-    private void UpdateBodyCount()
-    {
-        bodyCount++;
-    }
-
     private void SetPositionList()
     {
         positionList.Enqueue(transform.position);
 
-        if (positionList.Count > stackLength * bodyCount)
+        if (positionList.Count > stackLength * bodyList.Count)
         {
             positionList.Dequeue();
         }
     }
 
-    // Fonction pour obtenir les positions enregistrées pendant une seconde
+    //Fonction pour obtenir les positions enregistrées pendant une seconde
     public Queue<Vector3> GetPositionList()
     {
         return positionList;
-    }
-
-    public int GetBodyCount()
-    {
-        return bodyCount;
     }
 
     private void OnTriggerEnter(Collider collider)
@@ -76,14 +64,34 @@ public class HeadControl : MonoBehaviour
         if (collider.gameObject.CompareTag("Collectable")) // Vérifie si l'objet collisionné est bien "coins"
         {
             Destroy(collider.gameObject); // Détruit l'objet "coins"
-            UpdateBodyCount();
-            GameObject newBody = Instantiate(bodyPrefab, transform.position, transform.rotation);
+      
+            GameObject newBody = Instantiate(bodyPrefab, CalculateNewBodyPosition(), transform.rotation);
 
             BodyController bc = newBody.GetComponent<BodyController>();
+            bodyList.Add(newBody);
             bc.headControl = this;
             bc.queueIndex = bodyList.Count + 1;
 
-            bodyList.Add(newBody);
+        }
+    }
+
+    private Vector3 CalculateNewBodyPosition()
+    {
+        // Vérifie si des corps existent déjà dans la chaîne
+        if (bodyList.Count > 0)
+        {
+            // Récupère la position du dernier Body dans la chaîne
+            Vector3 lastBodyPosition = bodyList[bodyList.Count - 1].transform.position;
+
+            // Calcule une nouvelle position derrière le dernier Body dans la direction opposée à celle du mouvement du Head
+            Vector3 newBodyPosition = lastBodyPosition - transform.forward * bodySpacing;
+
+            return newBodyPosition;
+        }
+        else
+        {
+            // Si aucun corps n'existe encore dans la chaîne, place le nouveau Body directement derrière la tête
+            return transform.position - transform.forward * bodySpacing;
         }
     }
 }
