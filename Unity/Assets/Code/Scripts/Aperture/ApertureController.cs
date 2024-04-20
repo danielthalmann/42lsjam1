c40cefc07cc00219ec7f8584932d4c804099c5e1
Daniel Thalmann <daniel@thalmann.li>
2024-04-20 14:44:58 +0200

    add animation fade in fade out

---
+++ Unity/Assets/Code/Scripts/Aperture/ApertureController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GameManager;
+
+public class ApertureController : MonoBehaviour
+{
+    public Animator animatorFadeIn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.onStateChange += OnStateChange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnStateChange(GameState state)
+    {
+        if (state == GameState.init)
+        {
+            animatorFadeIn.SetTrigger("FadeIn");
+        }
+    }
+
+
+}
