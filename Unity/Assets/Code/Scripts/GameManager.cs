4f2420316de7b153127d24519221638da59678d1
Daniel Thalmann <daniel@thalmann.li>
2024-04-20 14:45:37 +0200

    updade gamemanager

--- Unity/Assets/Code/Scripts/GameManager.cs
+++ Unity/Assets/Code/Scripts/GameManager.cs
@@ -2,10 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static GameManager;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public delegate void OnCollectableChange(int value);
+    public OnCollectableChange onCollacteChange;
+
+    public delegate void OnStateChange(GameState state);
+    public OnStateChange onStateChange;
+
+    public int collected = 0;
+
+    public enum GameState
+    {
+        init,
+        start,
+        win,
+        loose
+    }
+
+    public GameState state = GameState.init;
+
 
     private void Awake()
     {
@@ -15,7 +35,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitGame();
     }
 
     // Update is called once per frame
@@ -24,14 +44,59 @@ public class GameManager : MonoBehaviour
                 
     }
 
+    /// <summary>
+    /// Quit l'application
+    /// </summary>
     public void Quit()
     {
         Application.Quit();
     }
 
+    /// <summary>
+    /// Charge un nouveau niveau
+    /// </summary>
+    /// <param name="level"></param>
     public void loadLevel(string level)
     {
         SceneManager.LoadScene(level);
     }
+
+    /// <summary>
+    /// Effectue 
+    /// </summary>
+    /// <param name="newState"></param>
+    public void ChangeState(GameState newState)
+    {
+        state = newState;
+        onStateChange?.Invoke(state);
+    }
+
+    /// <summary>
+    /// initialise le jeu
+    /// </summary>
+    public void InitGame()
+    {
+        collected = 0;
+        ChangeState(GameState.init);
+    }
+
+    /// <summary>
+    /// Indique que le jeu est perdu
+    /// </summary>
+    public void GameLoose()
+    {
+        ChangeState(GameState.loose);
+    }
+
+    /// <summary>
+    /// indique le nombre d'élément collecté
+    /// </summary>
+    /// <param name="value"></param>
+    public void Collecte(int value = 1)
+    {
+        collected++;
+        onCollacteChange?.Invoke(collected);
+    }
+
 
 }
